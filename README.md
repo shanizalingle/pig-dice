6cf21fee45831e10a880df1bec4b5e63c82afaa5
Shaniza Lingle <shanizalingle@gmail.com>
2022-08-24 15:46:16 -0700

    WIP: make caclulate function
    Co-authored-by: Kelly Bruce <kelly.bruce.cs@gmail.com>

--- README.md
+++ README.md
@@ -9,7 +9,12 @@ describe: player.prototype.addPlayer = function(player)
 
 test: this will create a unique ID for each player
 Code: this.currentId += 1
-Expected output: Player 1 = 1, Player 2 = 2
+Expected output: Player 1 = 1, Player 2 = 2]
+
+
+
+
+
 
 ----------------------------------------------
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
+function Game() {
+  this.players = {};
+  this.currentId = 0;
+}
+
 function rollTheDice(min, max) {
   min = Math.ceil(1);
   max = Math.floor(6);
   dice = Math.floor(Math.random() * (max - min + 1) + min);
   return dice;
 }
-
-
 
 function Player(playerId) {
   this.addedPoints = 0;
@@ -14,8 +17,8 @@ function Player(playerId) {
 }
 
 Player.prototype.calculate = function(){ 
-  let roll = dice;
-  if (roll === 1){ 
+  let roll = rollTheDice(1, 6);
+  if (roll === 1) { 
     this.addedPoints = 0;
   } else if (roll > 1) {
   this.totalPoints += roll;
