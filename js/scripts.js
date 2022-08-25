cd445a9fe5a08a4cab5c4620b1162beb74cac783
Shaniza Lingle <shanizalingle@gmail.com>
2022-08-24 21:35:36 -0700

    WIP: troubleshoot PlayerList function
    Co-authored-by: Kelly Bruce <kelly.bruce.cs@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,41 @@
-function Game() {
+function PlayerList() {
   this.players = {};
   this.currentId = 0;
 }
 
-function rollTheDice(min, max) {
+PlayerList.prototype.addPlayer = function(player) {
+  this.players[player.id] = player;
+}
+
+PlayerList.prototype.findPlayer = function(id) {
+  if (this.players[id] !== undefined) {
+    return this.players[id]
+  }
+  return false;
+}
+
+PlayerList.prototype.rollTheDice = function(min, max) {
   min = Math.ceil(1);
   max = Math.floor(6);
   dice = Math.floor(Math.random() * (max - min + 1) + min);
   return dice;
 }
 
+PlayerList.prototype.calculate = function(){ 
+  let roll = rollTheDice(1, 6);
+  console.log(roll);
+  if (roll === 1) { 
+    this.addedPoints = 0;
+    return this.addedPoints;
+  } else if (roll > 1) {
+    this.totalPoints += roll;
+    return this.totalPoints;
+  }
+  return roll;
+}
+
 function Player(playerId) {
   this.addedPoints = 0;
   this.totalPoints = 0;
   this.playerId = playerId;
-}
-
-Player.prototype.calculate = function(){ 
-  let roll = rollTheDice(1, 6);
-  if (roll === 1) { 
-    this.addedPoints = 0;
-  } else if (roll > 1) {
-  this.totalPoints += roll;
-}
-console.log("roll");
-return roll;
 }
